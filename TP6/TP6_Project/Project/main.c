b5bb8c056ff2618c605e0c01012e8547bcf8d3e6
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-08 21:20:30 +0200

    odometry testing

--- TP6/TP6_Project/Project/main.c
+++ TP6/TP6_Project/Project/main.c
@@ -17,6 +17,24 @@
 #include "moving.h"
 #include "read_image.h"
 
+//Distances parameters
+#define PI                 			3.1415926536f
+#define WHEEL_PERIMETER     		13 		//[cm]
+#define NSTEP_ONE_TURN      		1000	// number of step for 1 turn of the motor
+#define WHEEL_DISTANCE      		5.30f    //cm
+#define PERIMETER_EPUCK     		(PI * WHEEL_DISTANCE)
+
+
+static	int x = 0;
+static	int y = 0;
+static int old_pos_r = 0;
+static int old_pos_l = 0;
+
+void motor_set_position_test(float position_r, float position_l, int16_t speed_r, int16_t speed_l);
+int cm_to_step_test (float cm);
+int step_to_cm_test (int step);
+int16_t cms_to_steps_test (int16_t speed_cms);
+
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
@@ -67,45 +85,150 @@ int main(void)
 	 * send_params : NO_VISUALIZE_PARAMS, VISUALIZE_PARAMS
 	 */
 
-	rgb_gain_t rgb_gains = {94, 80, 80};
-	uint8_t contrast = 85;
-	//tuning uses line_index_top for plot visualization
-	uint16_t line_index_top = 10;
-	detect_mode_t mode_detect = MAX_ONLY;
-	visualize_mode_t send_params = YES_VISUALIZE_PARAMS;
-#ifdef TUNE
-	//chose which color intensity to plot with plot_image.py
-	color_index_t plot_pixels_color = BLUE_IDX;
-#else
-	uint16_t line_index_bot = 400;
-#endif
-
-
-	//TUNE is defined in main.h
-#ifdef TUNE
-	//Contrast level, camera line index, detect_mode, image color, visualize parameters such as means, maxs, counts on terminal
-	//Adjust Contrast, Line_Idx and detection mode  in Main.h
-	tuning_config_t tunning = {rgb_gains, contrast, line_index_top, mode_detect, plot_pixels_color, send_params};
-	tune_image_start(tunning);
-#else
-	//inits the motors
+//	rgb_gain_t rgb_gains = {94, 80, 80};
+//	uint8_t contrast = 85;
+//	//tuning uses line_index_top for plot visualization
+//	uint16_t line_index_top = 10;
+//	detect_mode_t mode_detect = MAX_ONLY;
+//	visualize_mode_t send_params = YES_VISUALIZE_PARAMS;
+//#ifdef TUNE
+//	//chose which color intensity to plot with plot_image.py
+//	color_index_t plot_pixels_color = BLUE_IDX;
+//#else
+//	uint16_t line_index_bot = 400;
+//#endif
+//
+//
+//	//TUNE is defined in main.h
+//#ifdef TUNE
+//	//Contrast level, camera line index, detect_mode, image color, visualize parameters such as means, maxs, counts on terminal
+//	//Adjust Contrast, Line_Idx and detection mode  in Main.h
+//	tuning_config_t tunning = {rgb_gains, contrast, line_index_top, mode_detect, plot_pixels_color, send_params};
+//	tune_image_start(tunning);
+//#else
+//	//inits the motors
 	motors_init();
+//
+//	config_t config = {rgb_gains, contrast, line_index_top, line_index_bot, mode_detect, send_params};
+//	read_image_start(config);
+//
+//	proximity_start();
+//
+//	moving_start();
+//
+//
+//#endif
 
-	config_t config = {rgb_gains, contrast, line_index_top, line_index_bot, mode_detect, send_params};
-	read_image_start(config);
+//	while (1) {
+//
+//
+//
+//		//waits 1 second
+//		chThdSleepMilliseconds(1000);
+//	}
 
-	proximity_start();
-
-	moving_start();
 
 
-#endif
 
-	while (1) {
-		//waits 1 second
-		chThdSleepMilliseconds(1000);
+
+
+	while(1){
+
+		x = 0;
+		y = 0;
+
+		old_pos_r = 0;
+		old_pos_l = 0;
+
+		left_motor_set_pos(0);
+		right_motor_set_pos(0);
+
+		motor_set_position_test(10, 10, 4, 4);
+		chThdSleepMilliseconds(2000);
+		motor_set_position_test(PERIMETER_EPUCK/2,PERIMETER_EPUCK/2, 3,-3);
+		chThdSleepMilliseconds(2000);
+		motor_set_position_test(10, 10, 4, 4);
+		chThdSleepMilliseconds(2000);
+		motor_set_position_test(PERIMETER_EPUCK/2,PERIMETER_EPUCK/2, 3,-3);
+		chThdSleepMilliseconds(5000);
+	}
+
+}
+
+
+static uint8_t position_reached = 0;
+
+void motor_set_position_test(float position_r, float position_l, int16_t speed_r, int16_t speed_l){
+
+	position_reached = 0;
+
+	int d_center = 0;
+	int theta = 0;
+
+	left_motor_set_pos(0);
+	right_motor_set_pos(0);
+
+	int position_to_reach_left = cm_to_step_test(position_l);
+	int position_to_reach_right = - cm_to_step_test(position_r);
+
+	left_motor_set_speed(cms_to_steps_test(speed_l));
+	right_motor_set_speed(cms_to_steps_test(speed_r));
+
+	while (!position_reached){
+
+		d_center = (right_motor_get_pos() + left_motor_get_pos())/2;
+		theta = (right_motor_get_pos() - left_motor_get_pos())/WHEEL_DISTANCE;
+
+		x = d_center*cos(theta);
+		y = d_center*sin(theta);
+
+		chprintf((BaseSequentialStream *)&SD3, "x =%-7d y =%-7d theta =%-7d \r\n\n",
+							step_to_cm_test(x),step_to_cm_test(y), theta);
+
+		chprintf((BaseSequentialStream *)&SD3, "rpos =%-7d lpos =%-7d \r\n\n",
+				right_motor_get_pos(),left_motor_get_pos());
+
+
+		if (abs(right_motor_get_pos()) > abs(position_to_reach_right) && abs(left_motor_get_pos()) > abs(position_to_reach_left) ){
+			left_motor_set_speed(0);
+			right_motor_set_speed(0);
+			position_reached = 1;
+
+			d_center = (right_motor_get_pos() + old_pos_r + left_motor_get_pos() + old_pos_l)/2;
+			theta = (right_motor_get_pos() + old_pos_r - left_motor_get_pos() - old_pos_l)/WHEEL_DISTANCE;
+
+			x = d_center*cos(theta);
+			y = d_center*sin(theta);
+
+			old_pos_r = old_pos_r + right_motor_get_pos();
+			old_pos_l = old_pos_l + left_motor_get_pos();
+
+
+			chprintf((BaseSequentialStream *)&SD3, "x =%-7d y =%-7d theta =%-7d \r\n\n",
+										step_to_cm_test(x),step_to_cm_test(y), theta);
+
+					chprintf((BaseSequentialStream *)&SD3, "rpos =%-7d lpos =%-7d \r\n\n",
+							right_motor_get_pos(),left_motor_get_pos());
+		}
 	}
 }
+
+
+
+
+int step_to_cm_test (int step) {
+	return step * WHEEL_PERIMETER / NSTEP_ONE_TURN;
+}
+
+int16_t cms_to_steps_test (int16_t speed_cms) {
+	return speed_cms * NSTEP_ONE_TURN / WHEEL_PERIMETER;
+}
+
+int cm_to_step_test (float cm) {
+	return (int)(cm * NSTEP_ONE_TURN / WHEEL_PERIMETER);
+}
+
+
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
