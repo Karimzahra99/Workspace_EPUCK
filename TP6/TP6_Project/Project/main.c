915954153588ce3509538b4cdd7f835d9e5df600
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-13 11:16:17 +0200

    Update main.c

--- TP6/TP6_Project/Project/main.c
+++ TP6/TP6_Project/Project/main.c
@@ -71,7 +71,7 @@ int main(void)
 	uint8_t contrast = 100;
 	//tuning uses line_index_top for plot visualization
 	uint16_t line_index_top = 10;
-	detect_mode_t mode_detect = RAINY_DAY;
+	detect_mode_t mode_detect = MAX_ONLY;
 	visualize_mode_t send_params = YES_VISUALIZE_PARAMS;
 #ifdef TUNE
 	//chose which color intensity to plot with plot_image.py
